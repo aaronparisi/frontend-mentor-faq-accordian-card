352ca7d6d00556874b0c4bad72d859d14353c6a5
aaronparisi <parisi.aaron@gmail.com>
2022-04-16 14:42:40 -0700

    faq list click functionality, basic icon transform

--- src/components/FAQCard/FAQItemComponent.tsx
+++ src/components/FAQCard/FAQItemComponent.tsx
@@ -1,10 +1,36 @@
 import React, { Dispatch, SetStateAction, ReactElement, useState } from "react";
+import styled from "styled-components";
+import colors from "../../stylesheets/_export.scss";
+import { MdKeyboardArrowDown } from "react-icons/md";
 
 interface FAQItemComponentProps {
   question: string;
   answer: string;
   setFaqSelected: Dispatch<SetStateAction<boolean>>;
 }
+
+interface QuestionProps {
+  isSelected: boolean;
+}
+
+const Question = styled("div")<QuestionProps>`
+  font-weight: ${({ isSelected }): string => (isSelected ? "bold" : "normal")};
+  &:hover {
+    cursor: ${({ isSelected }): string => (isSelected ? "default" : "pointer")};
+  }
+  &:hover {
+    color: ${({ isSelected }): string =>
+      isSelected ? "black" : `${colors.orange}`};
+  }
+`;
+
+interface AnswerProps {
+  isSelected: boolean;
+}
+
+const Answer = styled("div")<AnswerProps>`
+  display: ${({ isSelected }): string => (isSelected ? "block" : "none")};
+`;
 
 const FAQItemComponent: React.FC<FAQItemComponentProps> = ({
   question,
@@ -21,10 +47,21 @@ const FAQItemComponent: React.FC<FAQItemComponentProps> = ({
   };
 
   return (
-    <div className="faq-item-component" onClick={() => handleClick()}>
-      <p className="question">{question}</p>
-      <p className="answer">{answer}</p>
-    </div>
+    <li className="faq-item-component" onClick={() => handleClick()}>
+      <Question className="question" isSelected={isSelected}>
+        {question}
+      </Question>
+      <Answer className="answer" isSelected={isSelected}>
+        {answer}
+      </Answer>
+      <MdKeyboardArrowDown
+        className="open-close-arrow"
+        style={{
+          color: colors.orange,
+          transform: isSelected ? "rotate(180deg)" : "none",
+        }}
+      />
+    </li>
   );
 };
 
